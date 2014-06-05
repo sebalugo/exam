d8c6b5524fbe4429617a57ba6941bf9053cc43f0
root <root@ubuntu.(none)>
2014-06-05 07:00:48 -0700

    Initial Commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+["order", "product"].each do |f|
+	require File.join(File.dirname(__FILE__), '..', "lib/#{f}.rb")
+end
+
+require 'yaml'
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.formatter = :documentation
+end
