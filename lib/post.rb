a5ce1aacc0e5508f2ec390a52e88f024ad64d60f
root <root@ubuntu.(none)>
2014-06-05 07:18:41 -0700

    Implemented post class constructor

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,23 @@
 class Post
 
+attr_accessor :title,:text,:date,:user,:filename
+
+def initialize title=nil,text=nil,date=nil,user=nil,filename=nil
+
+	if filename.nil? 	
+	@title = title
+	@text = text
+	@date = date
+	@user = user
+	else
+	file = YAML::load(File.open(filename,"r"))
+	@title = file.title
+	@text = file.text
+	@date = file.date
+	@user = file.user
+	end
+	
+end
+
+
 end
